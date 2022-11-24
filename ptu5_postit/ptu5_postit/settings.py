ec8efd6b8b04bd24ee5b6758376f6cee57fd0f01
Sergejus Bykovas <sergejus83@gmail.com>
2022-11-24 10:21:19 +0200

    Created admin , made new posts; from admin browser

--- ptu5_postit/ptu5_postit/settings.py
+++ ptu5_postit/ptu5_postit/settings.py
@@ -32,7 +32,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'ptu5_postit',
+    'postit_api',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

7a7f5aa8e01ee333b8182e3d2413b0693e7acff6
Sergejus Bykovas <sergejus83@gmail.com>
2022-11-24 12:01:18 +0200

    post can only loged users; if not only for view

--- ptu5_postit/postit_api/views.py
+++ ptu5_postit/postit_api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, permissions
 from . import models, serializers
 
 
@@ -11,3 +11,8 @@ from . import models, serializers
 class PostList(generics.ListCreateAPIView):
     queryset = models.Post.objects.all()
     serializer_class = serializers.PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
